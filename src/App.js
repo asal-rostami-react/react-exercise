8a730e0c6c48b3aba907244ee6c672f83e154505
asal <asal.react@gmail.com>
2020-01-09 11:38:51 -0500

    added event and some attributes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+
 
 class App extends Component {
+
+  state = {
+    username: 'Siamak'
+
+  };
+
+  inputChangedHandler = (event) => {
+    this.setState({username: event.target.value})
+
+  }
+
   render() {
     return (
       <div className="App">
        <h1>react exercise</h1> 
+       <UserInput 
+       changed={this.inputChangedHandler}
+       currentName={this.state.username} />
+       <UserOutput userName="Asal" />
+       <UserOutput userName={this.state.username} />
+       <UserOutput userName="Asal" />
       </div>
     );
   }
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const userOutput = () => {
+const userOutput = (props) => {
 
     return(
         <div>
-            <p>Some random text!</p>
+            <p>Username: {props.userName}</p>
             <p>I hope I'll be overwritten!</p>
         </div>
 
